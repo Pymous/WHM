dc8943efeb37c183275d6d01f411aeabe26b916b
Pymous <pymous.studio@gmail.com>
2025-06-08 23:25:49 +0200

    Added a secondary warning to link Discord and added some links in the Quick Links section

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { Head, usePage } from '@inertiajs/vue3';
+
+const discordID = usePage().props.auth.user.discord_id;
 </script>
 
 <template>
@@ -10,12 +12,23 @@ import { Head, usePage } from '@inertiajs/vue3';
             <alert class="max-w-xs"> Work In Progress </alert>
         </div>
         <div class="max-md:order-1 lg:w-5/12">
+            <alert v-if="!discordID" class="mb-4" type="warning">
+                <b>Your Discord is not linked to WHM</b>
+                <br />
+                Please link your Discord account by clicking the Discord icon in the sidebar.
+            </alert>
             <!-- QUICK LINKS -->
             <div class="grid grid-cols-2 gap-4">
-                <a href="/" class="btn text-lg uppercase"> <i class="fas fa-book mr-4 text-xl"></i> Guides </a>
-                <a href="/" class="btn text-lg uppercase"> <i class="fas fa-rocket mr-4 text-xl"></i> Pathfinder </a>
-                <a href="/" class="btn text-lg uppercase"> <i class="fab fa-discord mr-4 text-xl"></i> Discord </a>
-                <a href="/" class="btn text-lg uppercase"> <i class="fas fa-chart-line mr-4 text-xl"></i> Janice </a>
+                <a href="https://help.fo2re.space/" target="_blank" class="btn text-lg uppercase">
+                    <i class="fas fa-book mr-4 text-xl"></i> Guides
+                </a>
+                <a href="https://pf.fo2re.space/" target="_blank" class="btn text-lg uppercase">
+                    <i class="fas fa-rocket mr-4 text-xl"></i> Pathfinder
+                </a>
+                <a :href="route('discord.auth.login')" class="btn text-lg uppercase"> <i class="fab fa-discord mr-4 text-xl"></i> Discord </a>
+                <a href="https://janice.e-351.com" target="_blank" class="btn text-lg uppercase">
+                    <i class="fas fa-chart-line mr-4 text-xl"></i> Janice
+                </a>
             </div>
             <!-- TOONS MANAGEMENT -->
             <h2 class="mt-8">MY TOONS</h2>
